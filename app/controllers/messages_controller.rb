7a4598d5ca8a65aedb492d29dda6dbccab15aac8
Andre Ferrer <andrerferrer@hotmail.com>
2021-11-30 14:37:09 +0100

    broadcast to notifications channel when creating messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,13 +2,15 @@ class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find params[:chatroom_id]
     @user = current_user
+    @other_users = @chatroom.users.distinct.where.not(id: current_user.id)
     @message = Message.new(strong_params)
     @message.user = @user
     @message.chatroom = @chatroom
 
     if @message.save
       redirect_to chatroom_path(@chatroom)
-      broadcast(@chatroom, @message)
+      chatroom_broadcast(@chatroom, @message)
+      notifications_broadcast(@other_users)
     else
       render 'chatrooms/show'
     end
@@ -20,10 +22,19 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content)
   end
 
-  def broadcast(chatroom, message)
+  def chatroom_broadcast(chatroom, message)
     ChatroomChannel.broadcast_to(
       chatroom,
       render_to_string(partial: "message", locals: { message: message })
     )
+  end
+
+  def notifications_broadcast(users)
+    users.each do |user|
+      NotificationChannel.broadcast_to(
+        user,
+        '1'
+      )
+    end
   end
 end
